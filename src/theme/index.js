a744ac04fd439947e9c18614cccab71d13ed57cc
yungshun317 <seraph317@gmail.com>
2021-06-21 14:13:43 +0800

    [CHORE] Have a theme for each `color`

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,5 +1,29 @@
 const themes = {
 	red: {
-		
+		background: "#ecaea4",
+		foreground: "#a62c42",
+		accent1: "#cb3e38",
+		accent2: "#db5c57",
+		accent3: "#862738",
+		text1: "#a5a757",
+		text2: "#4f804e"
+	}, 
+	blue: {
+		background: "#81c9e1",
+		foreground: "#bce1df",
+		accent1: "#bbe0ef",
+		accent2: "#1da4de",
+		accent3: "#fcd475",
+		text1: "#f2e8c8",
+		text2: "#cfcecd"
+	},
+	green: {
+		background: "#ede4ba",
+		foreground: "#b4c494",
+		accent1: "#348c40",
+		accent2: "#1a8367",
+		accent3: "#77bfb1",
+		text1: "#82584d",
+		text2: "#c7924c"
 	}
 }
