84116c95cf89ac367b117ff31676a6009cce6905
yungshun317 <seraph317@gmail.com>
2021-06-21 19:15:28 +0800

    [FEAT] Render `KitDropdown` and `KitDropdownItem` in the `Navigation` component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,31 @@
 import React from "react";
 
 import KitNavbar from "../kit/KitNavbar";
+import KitDropdown from "../kit/KitDropdown";
 
 export default function Navigation(props) {
+    const renderColor = (color, index) => {
+    	return (
+    		<KitDropdownItem
+    		    onSelect={() => props.setColorIndex(index)}
+    		    key={index}
+    		>
+    		    {color.title}
+    		</KitDropdownItem>
+    	);
+    };
+
+	const renderColorMenu = () => {
+		const { colors, colorIndex: current } = props;
+		return (
+			<KitDropdown
+			    alignRight
+			    label={colors[current].title}
+			    items={props.destinations.map((color, index) => renderItem(color, index))}
+			/>
+		);
+	};
+
 	return (
 		<KitNavbar></KitNavbar>
 	);
