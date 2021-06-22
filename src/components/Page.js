31f810047d64c044e0f6cfbc5e57f0a8a0efceb8
yungshun317 <seraph317@gmail.com>
2021-06-22 15:49:22 +0800

    [FEAT] Use Bootstrap's responsive grid system

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-import { KitContainer } from "../kit/KitContainer";
+import { KitContainer, KitRow, KitCol } from "../kit/KitContainer";
 
 export default function Page(props) {
 	return (
 		<KitContainer>
+		    <KitRow>
+		        <KitCol xl={2} lg={4} md={4} sm={4} xs={12}>
+		        </KitCol>
+		        <KitCol xl={10} lg={8} md={8} sm={8} xs={12}>
+		        </KitCol>
+		    </KitRow>
 		</KitContainer>
 	);
 }
