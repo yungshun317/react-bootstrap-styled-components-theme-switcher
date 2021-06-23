0f31a6581a55bf1a39fcedcf569991a8863d0bb7
yungshun317 <seraph317@gmail.com>
2021-06-23 19:21:36 +0800

    [FEAT] Create the `KitCarousel` component from Bootstrap `Carousel`

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Carousel from "./Carousel";
 import { KitContainer, KitRow, KitCol } from "../kit/KitContainer";
 
 export default function Page(props) {
@@ -7,6 +8,7 @@ export default function Page(props) {
 		<KitContainer>
 		    <KitRow>
 		        <KitCol xl={2} lg={4} md={4} sm={4} xs={12}>
+		            <Carousel {...props} />
 		        </KitCol>
 		        <KitCol xl={10} lg={8} md={8} sm={8} xs={12}>
 		        </KitCol>
---
+++ src/kit/KitCarousel.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import { Carousel } from "react-bootstrap";
+
+export const KitCarousel = styled(Carousel)``;
+
+export const KitCarouselItem = styled(Carousel.Item)``;
