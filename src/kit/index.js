ce7b9c38d70e57d0f4df055e9bebb63d1b0235c8
yungshun317 <seraph317@gmail.com>
2021-06-24 17:33:34 +0800

    [FEAT] Wrap all kit components up into a module

---
+++ src/kit/index.js
@@ -0,0 +1,19 @@
+import { KitGlobal } from "./KitGlobal";
+
+import { KitContainer, KitRow, KitCol } from "./KitContainer";
+import { KitCarousel, KitCarouselItem } from "./KitCarousel";
+import { KitNavbar } from "./KitNavbar";
+import { KitNav } from "./KitNav";
+import { KitNavDropdown } from "./KitNavDropdown";
+
+export {
+	KitGlobal,
+	KitCarousel,
+	KitCarouselItem,
+	KitContainer,
+	KitRow,
+	KitCol,
+	KitNavbar,
+	KitNav,
+	KitNavDropdown
+}
