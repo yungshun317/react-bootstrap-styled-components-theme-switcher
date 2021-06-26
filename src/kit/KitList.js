850d876755ea6743925344f2cb73c4d330e19f21
yungshun317 <seraph317@gmail.com>
2021-06-26 15:25:24 +0800

    [FEAT] Create the `KitList` component from Bootstrap `ListGroup`

---
+++ src/kit/KitList.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import { ListGroup } from "react-bootstrap";
+
+export const KitList = styled(ListGroup)``;
+
+export const KitListItem = styled(ListGroup.Item)``;
